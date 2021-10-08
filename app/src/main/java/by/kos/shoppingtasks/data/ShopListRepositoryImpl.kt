4e35fd06070f918c6b01d89f280a9b6063153bd5
Eugene Kos <mstrsid@live.com>
2021-10-08 12:15:24 +0300

    add data layer

---
+++ app/src/main/java/by/kos/shoppingtasks/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,36 @@
+package by.kos.shoppingtasks.data
+
+import by.kos.shoppingtasks.domain.ShopItem
+import by.kos.shoppingtasks.domain.ShopListRepository
+
+object ShopListRepositoryImpl : ShopListRepository {
+    private val shopList = mutableListOf<ShopItem>()
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if(shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(id: Int): ShopItem {
+        return shopList.find {
+            it.id == id
+        } ?: throw RuntimeException("Element with id $id not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+}
--- app/src/main/java/by/kos/shoppingtasks/domain/ShopItem.kt
+++ app/src/main/java/by/kos/shoppingtasks/domain/ShopItem.kt
@@ -1,9 +1,14 @@
 package by.kos.shoppingtasks.domain
 
 data class ShopItem(
-    val id: Int,
     val name: String,
     val count: Double,
     val measure: String,
-    val enabled: Boolean
+    val enabled: Boolean,
+    var id: Int = UNDEFINED_ID
 )
+{
+    companion object{
+        const val UNDEFINED_ID = -1
+    }
+}
